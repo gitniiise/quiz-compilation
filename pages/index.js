60534c384b0e100ab34b8618958344f95f9e312e
gitniiise <44769428+gitniiise@users.noreply.github.com>
2022-01-10 16:42:08 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
       </Head>
 
       <main>
@@ -15,9 +16,30 @@ export default function Home() {
         <p className="description">
           Hier steht ein cooler Text zu unserem Quiz für di Spieleleiter (dich)!
         </p>
-        <div class="box1">Quiz 1</div>
-        <div class="box2">Quiz 2</div>
-        <div class="box3">Quiz 3</div>
+        <div class="box2" style="
+    float: left;
+    width: 32%;
+    margin-right: 2%;
+    padding: 20px;
+    background: #eee;
+    box-sizing: border-box;
+">Quiz 2</div>
+  <div class="box2" style="
+    float: left;
+    width: 32%;
+    margin-right: 2%;
+    padding: 20px;
+    background: #eee;
+    box-sizing: border-box;
+">Quiz 2</div>
+        
+        <div class="w3-container w3-red w3-cell">
+          <p>Hello W3.CSS Layout.</p>
+        </div>
+
+        <div class="w3-container w3-green w3-cell">
+          <p>Hello W3.CSS Layout.</p>
+        </div>
       </main>
 
       <Footer />
